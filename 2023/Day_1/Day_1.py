326ebecf12bf342f2fefff63c2b6b0a47f91766e
Jamie-AE <jamie@aboutenergy.co.uk>
2023-12-02 10:29:35 +0000

    Fix to add 1a
    Fix to add 1a

--- 2023/Day_1/Day_1.py
+++ 2023/Day_1/Day_1.py
@@ -11,7 +11,8 @@ if filename == "":
     raise SystemExit("No txt file chosen")
 root1.destroy()
 
-calibration_values = []
+calibration_values_a = []
+calibration_values_b = []
 
 num = ['0','1','2','3','4','5','6','7','8','9']
 spelled = ['zero','one','two','three','four','five','six','seven','eight','nine']
@@ -25,8 +26,11 @@ with open(filename,'r') as csvfile:
     for row in lines:
         row_subs = re.compile("|".join(map_dict.keys())).sub(lambda ele: map_dict[re.escape(ele.group(0))], row[0])
         row_rev_subs = re.compile("|".join(map_dict_rev.keys())).sub(lambda ele: map_dict_rev[re.escape(ele.group(0))], row[0][::-1])
-        digits = re.findall(r'\d+', row_subs)
-        digits_rev = re.findall(r'\d+', row_rev_subs)
-        calibration_values.append(int(digits[0][0] + digits_rev[0][0]))
+        digits_a = re.findall(r'\d+', row[0])
+        digits_rev_a = re.findall(r'\d+', row[0][::-1])
+        digits_b = re.findall(r'\d+', row_subs)
+        digits_rev_b = re.findall(r'\d+', row_rev_subs)
+        calibration_values_a.append(int(digits_a[0][0] + digits_rev_a[0][0]))
+        calibration_values_b.append(int(digits_b[0][0] + digits_rev_b[0][0]))
 
-print(sum(calibration_values))
+print(sum(calibration_values_a), sum(calibration_values_b))
